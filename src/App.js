c9c709a0cd16b9b662d22aa05e62fcfaf2e39525
RanaTeerth <tirthrana99@gmail.com>
2023-05-28 13:39:46 +0530

    Day2

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import CartPage from "./pages/CartPage";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import ProductPage from "./pages/ProductPage";
+import Register from "./pages/Register";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          Hello , My name is Rana Teerth 
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <>
+    <div className="text-red-500">
+      <p className="text-center font-bold text-xl">Welcome To React Page</p>
     </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" Component={Home} />
+          <Route path="/login" Component={Login} />
+          <Route path="/register" Component={Register} />
+          <Route path="/product-page" Component={ProductPage} />
+          <Route path="/cart-page" Component={CartPage} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
